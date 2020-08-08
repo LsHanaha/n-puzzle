e0b4e0e8f9aae03eb7dfc05c66cd9f10dab78695
Josh Robin <jrobin-h@il-b4.msk.21-school.ru>
2020-08-08 18:49:14 +0300

    started func

--- c_folder/a_star.c
+++ c_folder/a_star.c
@@ -1,3 +1,8 @@
-#include <sys/tree.h>
+#include "a_star.h"
+
+uint8_t *a_star(uint8_t *map, uint8_t *goal, uint32_t side_len, double (*h)(uint8_t *map, uint8_t *goal, uint32_t side_len))
+{
+    // oof
+}
 
 
