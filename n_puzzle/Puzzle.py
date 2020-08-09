f026ac5d4d964455b3eebc357f782e0c59666456
Josh Robin <jrobin-h@il-b4.msk.21-school.ru>
2020-08-09 15:54:29 +0300

    lt, f for Puzzle

--- n_puzzle/Puzzle.py
+++ n_puzzle/Puzzle.py
@@ -1,4 +1,10 @@
 class Puzzle(list):
+    def __init__(self, *args, **kwargs):
+        self.g = kwargs.pop("g")
+        self.h = kwargs.pop("h")
+        self.parent = kwargs.pop("parent")
+        super().__init__(*args, **kwargs)
+
     def __hash__(self):
         ids = []
         length = len(self)
@@ -10,3 +16,10 @@ class Puzzle(list):
             out += base * ids[i]
             out //= length
         return out
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+    @property
+    def f(self):
+        return self.g + self.h
