69102d311543fbf13ebb61022b59628e7247ffb0
Josh Robin <jrobin-h@il-b4.msk.21-school.ru>
2020-08-09 20:05:29 +0300

    T_T

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
-import pytest
+import cProfile
 from n_puzzle.a_star import a_star
 from n_puzzle.Puzzle import Puzzle
 from n_puzzle import euristics
 
 
-@pytest.mark.parametrize("puzzle", [
-    [0, 1, 2, 3],
-    [3, 8, 1, 7, 0, 6, 4, 5, 2],
-    [4, 3, 9, 14, 2, 13, 7, 11, 5, 0, 6, 12, 15, 8, 1, 10]
-])
 def test_can_reach_finish(puzzle):
     Puzzle.set_side_len(int(len(puzzle) ** 0.5))
     puzzle = Puzzle(puzzle, g=0, h=0, parent=None)
     goal = sorted(puzzle)
-    assert a_star(puzzle, euristics.hemming) == goal
+    #assert a_star(puzzle, euristics.hemming) == goal
     assert a_star(puzzle, euristics.manhattan) == goal
+
+cProfile.run("test_can_reach_finish([14, 13, 0, 5, 8, 10, 3, 11, 15, 9, 6, 2, 12, 7, 4, 1])")
