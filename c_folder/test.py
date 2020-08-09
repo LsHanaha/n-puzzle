416b35ae37ce33ba3fe205dc2628daa0a2678c6b
Josh Robin <jrobin-h@il-b4.msk.21-school.ru>
2020-08-09 15:13:38 +0300

    adsfadfqfa

--- c_folder/test.py
+++ c_folder/test.py
@@ -1,16 +1,46 @@
 from itertools import permutations
+from math import factorial
 
 lst = list(range(9))
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
 
-def myhash(lst):
-    hashh = 0
-    for i, j in zip(lst, primes):
-        hashh += i * j
-    return hashh
+"""
+function identify_permutation(perm, chars) {
 
+    for (i = 0; i < length(perm); i++) {
+        ids[i] = get_index(perm[i], chars);
+    }
+
+    len = length(perm);
+    num_chars = length(chars);
+
+    index = 0;
+    base = num_chars ^ len - 1;
+    for (i = 0; i < length(perm); i++) {
+        index += base * ids[i];
+        base = base / len;
+    }
+
+}
+"""
+
+def myhash(lst):
+    ids = []
+    for elem in lst:
+        ids.append(elem)
+    index = 0
+    base = len(lst) ** len(lst) - 1
+    for i in range(len(lst)):
+        index += base * ids[i]
+        base //= len(lst)
+    return index
+    
+fac = factorial(13)
 hashes = dict()
 for perm in permutations(lst):
     hashh = myhash(perm)
     assert hashh not in hashes, f"{hashh}, {hashes[hashh]}, {perm}"
+    assert hashh < fac
+    print(hashh)
     hashes[hashh] = perm
+print(fac)
