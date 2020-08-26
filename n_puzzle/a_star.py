24b30436fafe346d748561fa80a8e3b219634a99
LsHanaha <you@example.com>
2020-08-26 21:24:58 +0300

    refactor a_start

--- n_puzzle/a_star.py
+++ n_puzzle/a_star.py
@@ -8,28 +8,23 @@ from typing import Callable, Optional
 
 def a_star(current: Puzzle, heuristic: Callable) -> Optional[Puzzle]:
     opened = []
+
+    max_opened = 0
+    total_closed = 0
+
     current.h = heuristic(current)
-    werty = 9999
     if not current.h:
         return current
     heappush(opened, current)
-
     closed = {}
-    if current in opened:
-        print("YEP!")
-    print("START heuristics = ", current.h)
-    while opened:
 
+    while opened:
         current = heappop(opened)
         current_id = current.hash
         closed[current_id] = current
         for neighbour in get_neighbours(current):
             neighbour_id = neighbour.hash
             neighbour.h = heuristic(neighbour)
-            if neighbour.h < werty or neighbour.h <= 5:
-                print(neighbour.h)
-                werty = neighbour.h
-                print(neighbour.board)
 
             if not neighbour.h:
                 return neighbour
@@ -39,8 +34,7 @@ def a_star(current: Puzzle, heuristic: Callable) -> Optional[Puzzle]:
                     continue
                 closed[neighbour_id].g = current.g + 1
                 closed[neighbour_id].parent = current
-            if neighbour in opened:
-                print("it's in opened")
+
             if neighbour not in opened:
                 heappush(opened, neighbour)
     raise BoardError("SOLUTION DIN't FOUND?!!!")
